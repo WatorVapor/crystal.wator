37156f1541784ad8611ec69f8c30243749375f6d
Wator Vapor <watorvapor@gmail.com>
2018-05-12 10:09:22 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,5 @@ const Wallet  = require('./wallet.js');
 let myWallet = new Wallet('wallet.dat');
 let api = new ApiWS(myWallet);
 
+let addressAll = myWallet.getAllAddress();
+console.log('addressAll=<',addressAll,'>');
