3536fb483db7fc944fd03335a8d71926dcb59be3
Wator Vapor <watorvapor@gmail.com>
2018-05-22 11:41:03 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ const Wallet  = require('./wallet.js');
 let myWallet = new Wallet('wallet.dat');
 let api = new ApiWS(myWallet);
 
+/*
 let addressAll = myWallet.getAllAddress();
 console.log('addressAll=<',addressAll,'>');
 let first = myWallet.signTransaction(addressAll[0],'test');
 console.log('first=<',first,'>');
+*/
+/*
+myWallet.pay(1000,'')
+*/
