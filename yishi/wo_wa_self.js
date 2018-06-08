6e16b714503d988be922d1df7a6e89ccbfd94001
Wator Vapor <watorvapor@gmail.com>
2018-06-08 20:24:35 +0900

    Update wo_wa_self.js

--- yishi/wo_wa_self.js
+++ yishi/wo_wa_self.js
@@ -112,7 +112,7 @@ module.exports = class WoWaSelf {
   mineTimeStamp_() {
     while(true) {
       let now = new Date();
-      let timestamp = now.toUTCString() + ' + ' now.getUTCMilliseconds() + 'ms';
+      let timestamp = now.toUTCString() + '.' + now.getUTCMilliseconds() + 'ms';
       let signatureTS = this.prvRSA.sign(timestamp,'base64');
       let derSignTS = Buffer.from(signatureTS.toDER()).toString('base64'); 
       let d = new SHA3.SHA3Hash();
