8c4ab84381418956dbdc9ff4922daea2dce3b7b1
Wator Vapor <watorvapor@gmail.com>
2018-06-16 18:26:32 +0900

    Update wo_wa_self.js

--- yishi/wo_wa_self.js
+++ yishi/wo_wa_self.js
@@ -128,7 +128,10 @@ module.exports = class WoWaSelf {
       dT.update(timestamp);
       let origHash = dT.digest('hex');
       
-      let signatureTS = this.key.sign(origHash).toDER().toString('base64');
+      let signatureTS = this.key.sign(origHash);
+      console.log('mineTimeStamp_::signatureTS=<',signatureTS,'>');
+      signatureTS = signatureTS.toDER().toString('base64');
+      console.log('mineTimeStamp_::signatureTS=<',signatureTS,'>');
       
       let d = new SHA3.SHA3Hash();
       d.update(signatureTS);
