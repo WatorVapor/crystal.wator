64795a6dc2cd9f255df2b5b7ecff3f5a7aba7820
Wator Vapor <watorvapor@gmail.com>
2018-06-18 23:01:58 +0900

    Update level_task_pump.js

--- yishi/level_task_pump.js
+++ yishi/level_task_pump.js
@@ -1,52 +1,63 @@
 const dbBlockPathTodo = '/watorvapor/wai.storage/crystal.wator/cnwiki/todo/block';
 const dbBlockPathDone = '/watorvapor/wai.storage/crystal.wator/cnwiki/done/block';
 const level = require('level');
-let dbTodo = level(dbBlockPathTodo);
-let dbDone = level(dbBlockPathDone);
-let stream = dbTodo.createReadStream();
-//console.log('stream=<',stream,'>');
-stream.on('data', function (data) {
-  //console.log('data.key=<',data.key.toString('utf-8'),'>');
-  //console.log('data.value=<',data.value.toString('utf-8'),'>');
-  stream.pause();
-  let blockCid = data.key.toString('utf-8');
-  dbDone.get(blockCid, function (err, value) {
-    if (err) {
-      if (err.notFound) {
-        console.log('blockCid=<',blockCid,'>');
-        let taskJson = {block:blockCid,task:'wator.ipfs.ostrich.app'};
-        pubRedis.publish(redisPubChannel,JSON.stringify(taskJson));
-      } else {
-        throw err;
-      }
-    } else {
-      console.log('dbDone blockCid=<',blockCid,'>');
-      stream.resume();
-    }
-  });
-});
 
-stream.on('error', function (err) {
-  console.log('Oh my!', err);
-});
-stream.on('close', function () {
-  console.log('Stream closed');
-});
-stream.on('end', function () {
-  console.log('Stream ended');
-});
 
 function saveDoneDB(result) {
   //console.log('saveDoneDB result=<',result,'>');
-  dbDone.put(result.input,JSON.stringify(result));
 }
 
 module.exports = class LevelTaskPump {
   constructor() {
+    this.dbDone = level(dbBlockPathDone);
+    this.dbTodo = level(dbBlockPathTodo);
   }
-  fetchOne() {
+  fetchOne(onTodoBlock) {
+    if(this.dbTodo.isClosed()) {
+      this.dbTodo.open();
+    }
+    if(this.dbDone.isClosed()) {
+      this.dbDone.open();
+    }
+    let stream = this.dbTodo.createReadStream();
+    //console.log('stream=<',stream,'>');
+    stream.on('data', function (data) {
+      //console.log('data.key=<',data.key.toString('utf-8'),'>');
+      //console.log('data.value=<',data.value.toString('utf-8'),'>');
+      stream.pause();
+      let blockCid = data.key.toString('utf-8');
+      this.dbDone.get(blockCid, function (err, value) {
+        if (err) {
+          if (err.notFound) {
+            console.log('blockCid=<',blockCid,'>');
+            onTodoBlock(blockCid);
+            this.dbDone.close();
+            this.dbTodo.close();
+          } else {
+            throw err;
+          }
+        } else {
+          console.log('dbDone blockCid=<',blockCid,'>');
+          stream.resume();
+        }
+      });
+    });
+    stream.on('error', function (err) {
+      console.log('Oh my!', err);
+    });
+    stream.on('close', function () {
+      console.log('Stream closed');
+    });
+    stream.on('end', function () {
+      console.log('Stream ended');
+    });
   }
   saveDone(result) {
+    if(this.dbDone.isClosed()) {
+      this.dbDone.open();
+    }
+    this.dbDone.put(result.input,JSON.stringify(result));
+    this.dbDone.close();
   }
 }
   
