241f7a09276aa4ac62f08274adda611ae86619c9
Wator Vapor <watorvapor@gmail.com>
2018-06-20 23:48:25 +0900

    Update level_task_dispath.js

--- yishi/level_task_dispath.js
+++ yishi/level_task_dispath.js
@@ -4,26 +4,73 @@ const dbBlockPathDone = '/watorvapor/wai.storage/crystal.wator/cnwiki/done/block
 const level = require('level');
 
 let dbTodo = level(dbBlockPathTodo);
-let stream = dbTodo.createReadStream();
+let streamTodo = dbTodo.createReadStream();
 
 let gToDoCidList = [];
-
-stream.on('data', function (data) {
+streamTodo.on('data', function (data) {
   //console.log('data.key=<',data.key.toString('utf-8'),'>');
   //console.log('data.value=<',data.value.toString('utf-8'),'>');
   let blockCid = data.key.toString('utf-8');
-  stream.pause();
+  streamTodo.pause();
   gToDoCidList.push(blockCid);
-  stream.resume();  
+  streamTodo.resume();  
 });
-stream.on('error', function (err) {
+streamTodo.on('error', function (err) {
   console.log('Oh my!', err);
 });
-stream.on('close', function () {
+streamTodo.on('close', function () {
   console.log('Stream closed');
 });
-stream.on('end', function () {
+streamTodo.on('end', function () {
   console.log('Stream ended');
   console.log('Stream ended gToDoCidList=<',gToDoCidList,'>');
 });
+
+let dbDoing = level(dbBlockPathDoing);
+let streamDoing = dbDoing.createReadStream();
+
+let gDoingCidList = [];
+streamDoing.on('data', function (data) {
+  //console.log('data.key=<',data.key.toString('utf-8'),'>');
+  //console.log('data.value=<',data.value.toString('utf-8'),'>');
+  let blockCid = data.key.toString('utf-8');
+  streamDoing.pause();
+  gDoingCidList.push(blockCid);
+  streamDoing.resume();  
+});
+streamDoing.on('error', function (err) {
+  console.log('Oh my!', err);
+});
+streamDoing.on('close', function () {
+  console.log('Stream closed');
+});
+streamDoing.on('end', function () {
+  console.log('Stream ended');
+  console.log('Stream ended gDoingCidList=<',gDoingCidList,'>');
+});
+
+let dbDone = level(dbBlockPathDone);
+let streamDone = dbDone.createReadStream();
+
+let gDoneCidList = [];
+streamDone.on('data', function (data) {
+  //console.log('data.key=<',data.key.toString('utf-8'),'>');
+  //console.log('data.value=<',data.value.toString('utf-8'),'>');
+  let blockCid = data.key.toString('utf-8');
+  streamDone.pause();
+  gDoneCidList.push(blockCid);
+  streamDone.resume();  
+});
+streamDone.on('error', function (err) {
+  console.log('Oh my!', err);
+});
+streamDone.on('close', function () {
+  console.log('Stream closed');
+});
+streamDone.on('end', function () {
+  console.log('Stream ended');
+  console.log('Stream ended gDoingCidList=<',gDoneCidList,'>');
+});
+
+
 
