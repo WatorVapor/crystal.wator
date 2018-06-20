8aa40a17c874c4d9b7ab22ace0ec0889547efa34
Wator Vapor <watorvapor@gmail.com>
2018-06-21 00:00:21 +0900

    Update level_task_dispath.js

--- yishi/level_task_dispath.js
+++ yishi/level_task_dispath.js
@@ -3,74 +3,51 @@ const dbBlockPathDoing = '/watorvapor/wai.storage/crystal.wator/cnwiki/doing/blo
 const dbBlockPathDone = '/watorvapor/wai.storage/crystal.wator/cnwiki/done/block';
 const level = require('level');
 
-let dbTodo = level(dbBlockPathTodo);
-let streamTodo = dbTodo.createReadStream();
-
 let gToDoCidList = [];
-streamTodo.on('data', function (data) {
-  //console.log('data.key=<',data.key.toString('utf-8'),'>');
-  //console.log('data.value=<',data.value.toString('utf-8'),'>');
-  let blockCid = data.key.toString('utf-8');
-  streamTodo.pause();
-  gToDoCidList.push(blockCid);
-  streamTodo.resume();  
-});
-streamTodo.on('error', function (err) {
-  console.log('Oh my!', err);
-});
-streamTodo.on('close', function () {
-  console.log('streamTodo closed');
-});
-streamTodo.on('end', function () {
-  console.log('streamTodo ended');
-  console.log('streamTodo ended gToDoCidList=<',gToDoCidList,'>');
-});
-
-let dbDoing = level(dbBlockPathDoing);
-let streamDoing = dbDoing.createReadStream();
-
 let gDoingCidList = [];
-streamDoing.on('data', function (data) {
-  //console.log('data.key=<',data.key.toString('utf-8'),'>');
-  //console.log('data.value=<',data.value.toString('utf-8'),'>');
-  let blockCid = data.key.toString('utf-8');
-  streamDoing.pause();
-  gDoingCidList.push(blockCid);
-  streamDoing.resume();  
-});
-streamDoing.on('error', function (err) {
-  console.log('Oh my!', err);
-});
-streamDoing.on('close', function () {
-  console.log('streamDoing closed');
-});
-streamDoing.on('end', function () {
-  console.log('streamDoing ended');
-  console.log('streamDoing ended gDoingCidList=<',gDoingCidList,'>');
-});
-
-let dbDone = level(dbBlockPathDone);
-let streamDone = dbDone.createReadStream();
-
 let gDoneCidList = [];
-streamDone.on('data', function (data) {
-  //console.log('data.key=<',data.key.toString('utf-8'),'>');
-  //console.log('data.value=<',data.value.toString('utf-8'),'>');
-  let blockCid = data.key.toString('utf-8');
-  streamDone.pause();
-  gDoneCidList.push(blockCid);
-  streamDone.resume();  
-});
-streamDone.on('error', function (err) {
-  console.log('Oh my!', err);
-});
-streamDone.on('close', function () {
-  console.log('streamDone closed');
-});
-streamDone.on('end', function () {
-  console.log('streamDone ended');
-  console.log('streamDone ended gDoingCidList=<',gDoneCidList,'>');
-});
 
+
+function readDB2Array(path,out,cb) {
+  let db = level(path);
+  let stream = db.createReadStream();
+
+  stream.on('data', function (data) {
+    //console.log('data.key=<',data.key.toString('utf-8'),'>');
+    //console.log('data.value=<',data.value.toString('utf-8'),'>');
+    let blockCid = data.key.toString('utf-8');
+    stream.pause();
+    out.push(blockCid);
+    stream.resume();  
+  });
+  stream.on('error', function (err) {
+    console.log('Oh my!', err);
+  });
+  stream.on('close', function () {
+    console.log('streamTodo closed');
+  });
+  stream.on('end', function () {
+    console.log('stream ended');
+    console.log('stream ended out=<',out,'>');
+  });
+}
+
+setTimeout(function(){
+  readDB2Array(dbBlockPathTodo,gToDoCidList,onReadTodoFinnish);
+},1);
+
+function onReadTodoFinnish(){
+  console.log('onReadDoneFinnish gToDoCidList=<',gToDoCidList,'>');
+  readDB2Array(dbBlockPathDoing,gToDoCidList,onReadDoingFinnish);
+}
+
+function onReadDoingFinnish(){
+  console.log('onReadDoneFinnish gDoingCidList=<',gDoingCidList,'>');
+  readDB2Array(dbBlockPathDone,gToDoCidList,onReadDoneFinnish);
+}
+
+function onReadDoneFinnish(){
+  console.log('onReadDoneFinnish gDoneCidList=<',gDoneCidList,'>');
+}
 
 
