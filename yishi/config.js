0156f8975e6081afe10504e27f993bf0ff0f0e14
Wator Vapor <watorvapor@gmail.com>
2018-06-24 10:29:32 +0900

    Update config.js

--- yishi/config.js
+++ yishi/config.js
@@ -1,12 +1,14 @@
 const ipfsAPI = require('ipfs-api');
 const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5003');
 
+/*
 ipfs.config.get((err, config) => {
   if (err) {
     throw err
   }
   console.log(config)
 });
+*/
 
 ipfs.swarm.addrs(function (err, addrs) {
   if (err) {
