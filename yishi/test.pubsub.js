79a1985819c51ee71c070ecf2761330cda1ce89f
Wator Vapor <watorvapor@gmail.com>
2018-06-24 10:07:43 +0900

    Update test.pubsub.js

--- yishi/test.pubsub.js
+++ yishi/test.pubsub.js
@@ -1,1 +1,23 @@
+const ipfsAPI = require('ipfs-api');
+const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5002');
 
+const ipfsPubTopic = 'wai-test-pub';
+const ipfsSubTopic = 'wai-test-sub';
+
+
+const onRcvIpfsSub = (msg) => {
+  console.log('onRcvIpfsCatchTask msg=<',msg,'>');
+  console.log('onRcvIpfsCatchTask msg=<',msg.data.toString('utf8'),'>');
+  //console.trace();
+}
+ipfs.pubsub.subscribe(ipfsSubTopic, onRcvIpfsSub,(err) => {
+  if (err) {
+    throw err
+  }
+  console.log('subscribe ipfsSubTopic=<',ipfsSubTopic,'>');
+  ipfs.pubsub.publish(ipfsPubTopic, msgBuff, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
