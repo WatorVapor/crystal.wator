a741e622aac78e581cd575046f5f653b9d15bc53
Wator Vapor <watorvapor@gmail.com>
2018-06-27 08:44:20 +0900

    Update pubsub-bug-config-nodejs.js

--- ipfs-cluster/pubsub-bug-config-nodejs.js
+++ ipfs-cluster/pubsub-bug-config-nodejs.js
@@ -3,11 +3,11 @@
 module.exports = () => ({
   Addresses: {
     Swarm: [
-      '/ip4/0.0.0.0/tcp/4002',
-      '/ip4/127.0.0.1/tcp/4003/ws'
+      '/ip4/0.0.0.0/tcp/4006',
+      '/ip4/127.0.0.1/tcp/4007/ws'
     ],
-    API: '/ip4/127.0.0.1/tcp/5002',
-    Gateway: '/ip4/127.0.0.1/tcp/9090'
+    API: '/ip4/127.0.0.1/tcp/5004',
+    Gateway: '/ip4/127.0.0.1/tcp/9092'
   },
   Discovery: {
     MDNS: {
