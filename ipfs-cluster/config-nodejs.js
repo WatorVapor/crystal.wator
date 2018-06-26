b2183493ac0afefe4593295293d9bf9a1f4a9727
Wator Vapor <watorvapor@gmail.com>
2018-06-27 08:43:34 +0900

    Update config-nodejs.js

--- ipfs-cluster/config-nodejs.js
+++ ipfs-cluster/config-nodejs.js
@@ -3,11 +3,11 @@
 module.exports = () => ({
   Addresses: {
     Swarm: [
-      '/ip4/0.0.0.0/tcp/4002',
-      '/ip4/127.0.0.1/tcp/4003/ws'
+      '/ip4/0.0.0.0/tcp/4004',
+      '/ip4/127.0.0.1/tcp/4005/ws'
     ],
-    API: '/ip4/127.0.0.1/tcp/5002',
-    Gateway: '/ip4/127.0.0.1/tcp/9090'
+    API: '/ip4/127.0.0.1/tcp/5003',
+    Gateway: '/ip4/127.0.0.1/tcp/9091'
   },
   Discovery: {
     MDNS: {
