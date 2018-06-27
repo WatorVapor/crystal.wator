bf85462ae092df0c09c4ac8b24a23eaf19b17cbd
Wator Vapor <watorvapor@gmail.com>
2018-06-27 23:16:23 +0900

    Update test0.pubsub.js

--- yishi/test/test0.pubsub.js
+++ yishi/test/test0.pubsub.js
@@ -1,6 +1,6 @@
 const ipfsAPI = require('ipfs-api');
-const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5002');
-const ipfs2 = ipfsAPI('/ip4/127.0.0.1/tcp/5003');
+const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5003');
+const ipfs2 = ipfsAPI('/ip4/127.0.0.1/tcp/5004');
 
 const ipfsSubTopic = 'wai-test-sub';
 
