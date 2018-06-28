ef48cfd259ab4bc75e2ae0523adb0234d7891d4f
Wator Vapor <watorvapor@gmail.com>
2018-06-29 06:57:26 +0900

    Update wo_wa_p2p.js

--- yishi/wo_wa_p2p.js
+++ yishi/wo_wa_p2p.js
@@ -1,6 +1,34 @@
 const Room = require('ipfs-pubsub-room');
-const IPFS = require('ipfs');
+const WOWAIPFS = require('ipfs');
+
+const WOWA_IPFS_CONF = {
+  repo: '.ipfs_pubsub_room_data',
+  EXPERIMENTAL: {
+    pubsub: true
+  },
+  config: {
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
+  }
+};
+
+
 module.exports = class WoWaP2p {
   constructor() {
+    this.ipfs = new IPFS(WOWA_IPFS_CONF);
+    this.ipfs.on('ready', this.onInit);
+  }
+  onInit() {
+    let self = this;
+    this.ipfs.id( (err,identity)=>{
+      if (err) {
+        throw err
+      }
+      console.log('identity=<',identity,'>');
+      self.peer = identity.id;
+    });
   }
 };
