18e0a94c0fe20174474b3a445f306663ad643b70
Wator Vapor <watorvapor@gmail.com>
2018-06-29 07:07:28 +0900

    Update wo_wa_p2p.js

--- yishi/wo_wa_p2p.js
+++ yishi/wo_wa_p2p.js
@@ -18,7 +18,7 @@ const IPFS_CONF = {
 const SHA3  = require('sha3');
 let d = new SHA3.SHA3Hash();
 d.update('欢迎来到意识WoWa');
-const ROOM_NUM = d.digest('bin');
+const ROOM_NUM = d.digest('bin').toString('base64');
 console.log('ROOM_NUM=<',ROOM_NUM,'>');
 
 module.exports = class WoWaP2p {
