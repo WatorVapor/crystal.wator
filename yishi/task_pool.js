aed24a961b2f672fbb941a6796e38f42123c2ba2
Wator Vapor <watorvapor@gmail.com>
2018-06-29 14:02:13 +0900

    Update task_pool.js

--- yishi/task_pool.js
+++ yishi/task_pool.js
@@ -1,5 +1,5 @@
 const ipfsAPI = require('ipfs-api');
-const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5004');
+const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5003');
 
 ipfs.id( (err, identity) => {
   if (err) {
