1b06c5fbb812d0482fe6e43456e2d4497128bd0d
Wator Vapor <watorvapor@gmail.com>
2018-07-03 07:09:42 +0900

    Update private-config-nodejs.js

--- ipfs-cluster/private-config-nodejs.js
+++ ipfs-cluster/private-config-nodejs.js
@@ -2,10 +2,6 @@
 
 module.exports = () => ({
   Addresses: {
-    Swarm: [
-      '/ip4/127.0.0.1/tcp/24002',
-      '/ip4/127.0.0.1/tcp/24003/ws'
-    ],
     API: '/ip4/127.0.0.1/tcp/25002',
     Gateway: '/ip4/127.0.0.1/tcp/29090'
   },
