43cd198559c47c993fe7cc0161dac8447dcfa844
Wator Vapor <watorvapor@gmail.com>
2018-07-03 21:15:18 +0900

    Update channel.js

--- yishi/channel.js
+++ yishi/channel.js
@@ -2,4 +2,5 @@ exports.TASK = {};
 exports.TASK.CREATE = 'wai-task-create';
 exports.TASK.CATCH  = 'wai-task-catch';
 exports.BLOCK = {};
-exports.BLOCK.ANNOUNCE  = 'wai-block-announce';
+exports.BLOCK.CREATE  = 'wai-block-create';
+exports.BLOCK.VERIFY  = 'wai-block-verify';
