37152bf027621cc8d0920df3ad89dfb9f3c3791c
Wator Vapor <watorvapor@gmail.com>
2018-07-05 05:59:18 +0900

    Update knowledge_chain.js

--- yishi/knowledge_chain.js
+++ yishi/knowledge_chain.js
@@ -1,76 +1,7 @@
-const redis = require("redis");
-const redisPubChannel = 'wai.relay.ipfs.to.redis';
-const redisSubChannel = 'wai.relay.redis.to.ipfs';
-
 const SHA3  = require('sha3');
 const bs58 = require('bs58')
-
-
-//let dTask = new SHA3.SHA3Hash(224);
-//dTask.update('wator.ipfs.ostrich.app');
-//let taskHex = dTask.digest('hex');
-//const TaskID = bs58.encode(taskHex);
-const TaskID ='wator.ipfs.ostrich.app';
-console.log('TaskID=<',TaskID,'>');
-
-module.exports = class TaskWorker {
+module.exports = class KnowledgeChain {
   constructor() {
-    this.pub = redis.createClient();
-    this.pub.on("ready", (err) => {
-      if(err) {
-        throw err;
-      }
-    });
-    this.sub = redis.createClient();
-    let self = this;
-    this.sub.on("ready", (err) => {
-      if(err) {
-        throw err;
-      }
-      self.sub.subscribe(redisSubChannel);
-    });
-    this.sub.on("message", (channel, msg) => {
-      self._onMessage(channel, msg);
-    });
-    this._oneBlockWords = {};
-  }
-  out(cid) {
-    let taskJson = {block:cid,task:TaskID};
-    this.pub.publish(redisPubChannel,JSON.stringify(taskJson));
-  }
-  
-  _onMessage(channel, msg) {
-    //console.log('subRedis.on channel=<',channel,'>');
-    //console.log('subRedis.on msg=<',msg,'>');
-    let jsonMsg = JSON.parse(msg);
-    if(jsonMsg) {
-      //console.log('subRedis.on jsonMsg=<',jsonMsg,'>');
-      if(jsonMsg.word) {
-        this._collectWords(jsonMsg.word);
-      } else {
-        //console.log('subRedis.on jsonMsg=<',jsonMsg,'>');
-        if(typeof this.onReadyOneBlock === 'function') {
-          this.onReadyOneBlock(jsonMsg,this._oneBlockWords);
-          this._oneBlockWords = {};
-        }
-      }
-    }
-  }
-  _collectWords(words) {
-    //console.log('collectWords words=<',words,'>');
-    for(let i = 0;i < words.length ; i++) {
-      let wordRank = words[i];
-      //console.log('collectWords wordRank=<',wordRank,'>');
-      let keys = Object.keys(wordRank);
-      //console.log('collectWords keys=<',keys,'>');
-      let word = keys[0];
-      if(this._oneBlockWords[word]) {
-        this._oneBlockWords[word] += wordRank[word];
-        //console.log('collectWords oneBlockWors[word]=<',oneBlockWors[word],'>');
-      } else {
-        this._oneBlockWords[word] = wordRank[word];
-      }
-    }
   }
 }
 
