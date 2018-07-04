eccded0de17dc99d55668f59b19b2c08d7528a25
Wator Vapor <watorvapor@gmail.com>
2018-07-05 06:26:40 +0900

    Update knowledge_chain.js

--- yishi/knowledge_chain.js
+++ yishi/knowledge_chain.js
@@ -1,17 +1,25 @@
 const SHA3  = require('sha3');
 const bs58 = require('bs58')
+
+const KnowledgeVerifiedTimeMin = 2;
+
 module.exports = class KnowledgeChain {
   constructor() {
     this.blockList_ = {};
   }
   push(msg) {
-    console.log('push::msg=<',msg,'>');
+    //console.log('push::msg=<',msg,'>');
     let nounce = 'n_' + msg.output.nounce;
-    console.log('push::nounce=<',nounce,'>');
+    //console.log('push::nounce=<',nounce,'>');
     if(this.blockList_[nounce]) {
       let block = this.blockList_[nounce];
       block.output.ts_verified.push(msg.output.ts_verified);
-      console.log('push::block=<',block,'>');
+      //console.log('push::block=<',block,'>');
+      if(block.output.ts_verified.length >= KnowledgeVerifiedTimeMin) {
+        if(this.addKnowledge_(block)) {
+          delete this.blockList_[nounce];
+        }
+      }
     } else {
       let ts_verified = msg.output.ts_verified;
       delete msg.output.ts_verified;
@@ -19,7 +27,14 @@ module.exports = class KnowledgeChain {
       msg.output.ts_verified.push(ts_verified);
       this.blockList_[nounce] = msg;
     }
-    console.log('this.blockList_=<',JSON.stringify(this.blockList_),'>');
+    //console.log('this.blockList_=<',JSON.stringify(this.blockList_),'>');
+  }
+  
+  addKnowledge_(block) {
+    console.log('addKnowledge_::block=<',JSON.stringify(block,null,2),'>');
+    if(typeof this.onKnowBlock === 'function') {
+      this.onKnowBlock();
+    }
   }
 }
 
