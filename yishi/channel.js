b82adf5829602109c6b124d6b7d3790be0aa00a4
Wator Vapor <watorvapor@gmail.com>
2018-07-04 22:38:48 +0900

    Update channel.js

--- yishi/channel.js
+++ yishi/channel.js
@@ -2,6 +2,7 @@ exports.TASK = {};
 exports.TASK.WANT = 'wai-task-want';
 exports.TASK.CREATE = 'wai-task-create';
 exports.TASK.CATCH  = 'wai-task-catch';
+exports.TASK.DONE  = 'wai-task-done';
 exports.KNOWLEDGE = {};
 exports.KNOWLEDGE.CREATE  = 'wai-Knowledge-create';
 exports.KNOWLEDGE.VERIFY  = 'wai-Knowledge-verify';
