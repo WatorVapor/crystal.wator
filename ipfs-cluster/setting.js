f24b65ef9611d0d569ab9fa56c0a730036557231
Wator Vapor <watorvapor@gmail.com>
2019-01-24 22:20:17 +0900

    Update setting.js

--- ipfs-cluster/setting.js
+++ ipfs-cluster/setting.js
@@ -1,6 +1,13 @@
 const ipfsClient = require('ipfs-http-client');
 let ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5003');
 console.log('ipfs=<',ipfs,'>');
+ipfs.config.get((err, config) => {
+  if (err) {
+    throw err
+  }
+  console.log(config)
+});
+/*
 ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Origin', '["https://ipfs.wator.xyz", "http://127.0.0.1:5003", "https://webui.ipfs.io"]', (err) => {
   if (err) {
     throw err
@@ -12,5 +19,7 @@ ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Methods', '["PUT", "GET", 
     throw err
   }
 })
+*/
+
 
 
