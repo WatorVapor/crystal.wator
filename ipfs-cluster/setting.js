e78388cda77b42ed76c9f002b6f22b14d0d31be9
Wator Vapor <watorvapor@gmail.com>
2019-01-24 21:50:21 +0900

    Update setting.js

--- ipfs-cluster/setting.js
+++ ipfs-cluster/setting.js
@@ -1,3 +1,18 @@
 const ipfsClient = require('ipfs-http-client');
 let ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5003');
 console.log('ipfs=<',ipfs,'>');
+ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Origin', '["https://ipfs.wator.xyz", "http://127.0.0.1:5003", "https://webui.ipfs.io"]', (err) => {
+  if (err) {
+    throw err
+  }
+  // MDNS Discovery was set to false
+})
+
+ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Methods', '["PUT", "GET", "POST"]', (err) => {
+  if (err) {
+    throw err
+  }
+  // MDNS Discovery was set to false
+})
+
+
