e392364e163ca33bb5bddeedc9df7aaa4fbc5739
Wator Vapor <watorvapor@gmail.com>
2019-01-24 21:45:52 +0900

    Update setting.js

--- ipfs-cluster/setting.js
+++ ipfs-cluster/setting.js
@@ -1,2 +1,3 @@
 const ipfsClient = require('ipfs-http-client');
-let ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5001');
+let ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5003');
+console.log('ipfs=<',ipfs,'>');
