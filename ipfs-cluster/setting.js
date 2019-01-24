7134e2e72cb5aa570f093935dd135b7bd17b3369
Wator Vapor <watorvapor@gmail.com>
2019-01-24 22:23:51 +0900

    Update setting.js

--- ipfs-cluster/setting.js
+++ ipfs-cluster/setting.js
@@ -25,13 +25,14 @@ ipfs.config.get('API.HTTPHeaders',(err, config) => {
 });
 
 
-/*
+
 ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Origin', '["https://ipfs.wator.xyz", "http://127.0.0.1:5003", "https://webui.ipfs.io"]', (err) => {
   if (err) {
     throw err
   }
-})
+});
 
+/*
 ipfs.config.set('API.HTTPHeaders.Access-Control-Allow-Methods', '["PUT", "GET", "POST"]', (err) => {
   if (err) {
     throw err
