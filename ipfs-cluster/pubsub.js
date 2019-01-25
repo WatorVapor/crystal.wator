763330d9e1702b21789c835aedabbe44ece34bfe
Wator Vapor <watorvapor@gmail.com>
2019-01-25 09:19:44 +0900

    Update pubsub.js

--- ipfs-cluster/pubsub.js
+++ ipfs-cluster/pubsub.js
@@ -1,3 +1,9 @@
 const ipfsClient = require('ipfs-http-client');
 let ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5003');
-console.log('ipfs=<',ipfs,'>');
+//console.log('ipfs=<',ipfs,'>');
+ipfs.swarm.addrs( (err, addrs) =>{
+  if (err) {
+    throw err
+  }
+  console.log('ipfs.swarm.addrs addrs=<',addrs,'>');
+})
