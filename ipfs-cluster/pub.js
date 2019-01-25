1fab84c60c2a92e05a11ef1c954cc1a72ba48f07
Wator Vapor <watorvapor@gmail.com>
2019-01-25 21:53:09 +0900

    Update pub.js

--- ipfs-cluster/pub.js
+++ ipfs-cluster/pub.js
@@ -23,6 +23,12 @@ node.on('ready', () => {
   connectSwarm();
   doShowID();
 })
+node.on('error', (error) => {
+  console.log('error=<',error,'>');
+})
+node.on('start', () => {
+  console.log('Node started!')
+});
 
 const swarmAddr = multiaddr('/ip6/2400:2412:13e0:9d00:2ce:39ff:fece:132/tcp/4006/ipfs/QmcdUnSFqTE9rYC2w623heJyWFKNtsbDemhpv7MPCFvbLa');
 console.log('swarmAddr=<',swarmAddr,'>');
