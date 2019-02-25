6945a78d842277a6bf661ec2dafec653783d5c1c
Wator Vapor <watorvapor@gmail.com>
2019-02-25 22:48:23 +0900

    Create setting.js

---
+++ go-ipfs-cluster/setting.js
@@ -0,0 +1,9 @@
+const ipfsAPI = require('ipfs-http-client');
+const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
+
+ipfs.id((err, identity) =>{
+  if (err) {
+    throw err;
+  }
+  console.log('ipfs.id identity=<',identity,'>');
+});
