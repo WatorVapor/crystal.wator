b7ef1285c530d388285cb5cac0d579baf736de4f
Wator Vapor <watorvapor@gmail.com>
2019-02-25 22:58:01 +0900

    Update setting.js

--- go-ipfs-cluster/setting.js
+++ go-ipfs-cluster/setting.js
@@ -19,11 +19,23 @@ ipfs.config.get((err, config) => {
 })
 */
 
+ipfs.config.set('Addresses.API', '/ip4/127.0.0.1/tcp/5001', (err) => {
+  if (err) {
+    throw err
+  }
+})
+
 ipfs.config.get('Addresses.API',(err, config) => {
   if (err) {
     throw err
   }
   console.log('ipfs.config.get Addresses.API config=<',config,'>');
+})
+
+ipfs.config.set('Addresses.Gateway', '/ip4/127.0.0.1/tcp/8080', (err) => {
+  if (err) {
+    throw err
+  }
 })
 
 ipfs.config.get('Addresses.Gateway',(err, config) => {
