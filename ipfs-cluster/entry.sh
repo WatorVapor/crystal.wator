b4725f93e62596f5b5f5bd09b762e89ec3343e3a
Wator Vapor <watorvapor@gmail.com>
2019-03-10 18:33:53 +0900

    Update entry.sh

--- ipfs-cluster/entry.sh
+++ ipfs-cluster/entry.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-npm update --save
+npm install --save
 cp -f config-nodejs.js node_modules/ipfs/src/core/runtime/config-nodejs.js
 cp -f init-and-daemon.sh node_modules/ipfs/init-and-daemon.sh
 cd node_modules/ipfs && ./init-and-daemon.sh
