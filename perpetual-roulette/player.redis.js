cd21ccb93129969c7b94f83b70182ed39ed97fdc
Wator Vapor <watovapor@gmail.com>
2019-03-28 12:49:54 +0000

    roulette

---
+++ perpetual-roulette/player.redis.js
@@ -0,0 +1,14 @@
+const Seat = require('./round-table').Seat;
+//console.log('Seat=<',Seat,'>');
+let seat = new Seat();
+seat.onReady = () => {
+  //console.log('seat=<',seat,'>');
+}
+seat.subscribe((msg,from)=> {
+  onDealMsg(msg,from);
+});
+
+onDealMsg = (msg,from) => {
+  console.log('onDealMsg::msg=<',msg,'>');
+  console.log('onDealMsg::from=<',from,'>');
+}
